a1250f9c1e63836d69de5bc965d9b24a1c7d8147
waiyan <waiyan112@gmail.com>
2015-04-11 01:15:14 +0630

    first

---
+++ kayeezinmodule/src/androidTest/java/com/zayeezin/nsdeveloper_2/kayeezinmodule/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.zayeezin.nsdeveloper_2.kayeezinmodule;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
